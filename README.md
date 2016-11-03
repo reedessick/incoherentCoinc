92e0f5263ecf5e2b78a551309df59556cddeab08
Reed Essick <reed.essick@ligo.org>
2016-11-03 18:27:18 -0400

    some more work on setting up observations.py

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 the idea here is to use the "Realization" class to simulate experiments repeatedly. We can then call that class's methods to extract statistics for each realization. We feed these into a "Data" object which represents all the observations from a single realization. Our likelihood will then take in a "Data" object and the associated rates.
 
-Currently in the middle of implementing the "Realization" class...
+We should write some wrapper function that takes in a "Realization" and spits out a "Data" object, ensuring the "Data" object is built correctly and all that jazz. "Data" objects might need a reference to a "Realization", and then can compute the statistics they need on the fly...
+
+Currently in the middle of figuring out how to perform time-slides and count coincs efficiently for an arbitrary unspecified number of detectors... (~lib/observations.slideCoinc(trgs, tau, dt, dur))
--- lib/observations.py
+++ lib/observations.py
@@ -4,9 +4,11 @@ author      = "reed.essick@ligo.org"
 #-------------------------------------------------
 
 import numpy as np
+import copy
 
 #-------------------------------------------------
-### general utility methods
+### related to Poisson Distribution
+#-------------------------------------------------
 
 def poisson_dt( r ):
     """
@@ -19,7 +21,7 @@ def poisson_series( T, r ):
     returns a list of Trigger objects drawn from poisson series of rate="r" and duration="T"
     """
     t = 0
-    ans = []
+    ans = TrigList()
     while t < T:
         t += poisson_dt( r )
         if t > T: 
@@ -28,30 +30,159 @@ def poisson_series( T, r ):
     return ans
 
 #-------------------------------------------------
+### coincidence counting and such
+#-------------------------------------------------
+
+def coinc(trgs, tau):
+    """
+    find coincs between all triggers in trgs ( list of lists of Triggers )
+    assumes triggers are already chronologically ordered
+
+    returns the number of coincidences, does not modify trgs
+    """
+    Ndet = len(trgs)
+    Ns = [len(_) for _ in trgs] ### number of triggers in each stream
+    inds = [[0,0] for _ in xrange(Ndet-1)] ### the indecies for each stream (besides stream 0, which we treat separately)
+
+    count = 0
+    for trg in trgs[0]: ### iterate over triggers in "stream 0", counting the total number of coincs for each
+        t = trg.time ### time of interest
+
+        for n, (s, e) in enumerate(inds): ### iterate through the rest of the streams and find the idecies that bracket "t"
+            N = Ns[n] ### number of events in "stream n"
+
+            while (s<N) and (trgs[n][s].time < t-tau): ### start is too early, so we bump it
+                s += 1
+
+            if e < s: ### end must be at least as big as the start
+                e = s
+            inds[n][0] = s ### update start points
+
+            ### ensure that the start is still a good coinc
+            if trgs[n][s].time > t+tau:
+                inds[n][1] = e ### update end point
+                break ### no good coincs here! so we stop working and move to the next "steam 0" trigger
+
+            while (e+1<N) and (trgs[n][e+1].time < t+tau): ### end time is too early
+                e += 1
+            inds[n][1] = e ### update end point
+
+        else: ### there must be at least one participating trigger in all streams, otherwise we would have broken.
+            c = 1
+            for s, e in inds:
+                c *= (e-s+1) ### total number of coincs is the product of the number participating in each stream
+            count += c
+
+    return count
+
+def removeCoinc(trgs, tau):
+    """
+    rinds all coincs between all triggers in trgs and removes them from the lists
+    assumes chronological ordering
+
+    returns trgs including modifications (for symantic ease)
+    """
+    Ndet = len(trgs)
+    Ns = [len(_) for _ in trgs]
+    inds = [[0,0] for _ in xrange(Ndet)]
+
+    remove = [set() for _ in xrange(Ndet)] ### list of indicies to be removed
+
+    for ind, trg in enumerate(trgs[0]):
+        t = trg.time ### time of interest
+
+        for n, (s, e) in enumerate(inds): ### iterate through the rest of the streams and find the idecies that bracket "t"
+            N = Ns[n] ### number of events in "stream n"
+
+            while (s<N) and (trgs[n][s].time < t-tau): ### start is too early, so we bump it
+                s += 1
+
+            if e < s: ### end must be at least as big as the start
+                e = s
+            inds[n][0] = s ### update start points
+
+            ### ensure that the start is still a good coinc
+            if trgs[n][s].time > t+tau:
+                inds[n][1] = e ### update end point
+                break ### no good coincs here! so we stop working and move to the next "steam 0" trigger
+
+            while (e+1<N) and (trgs[n][e+1].time < t+tau): ### end time is too early
+                e += 1
+            inds[n][1] = e ### update end point
+
+        else: ### there must be at least one participating trigger in all streams, otherwise we would have broken.
+
+            rem[0].add(ind)
+            for rem, (s, e) in zip(remove[1:], inds): ### add indecies to be removed to sets
+                for i in xrange(s,e+1):
+                    rem.add(i)
+            
+    for n, rem in enumerate(remove): ### iterate through lists and remove indecies
+        for i in sorted(rem, reverse=True): ### start from the back and work forward
+            trgs[n].pop(i)
+
+    return trgs
+
+def slideCoinc(trgs, tau, dt, dur):
+    """
+    perform a cyclic time shift and computes coincs for a list of TrigLists
+    assumes time ordered trgs and maintains this
+
+    returns the number of coincidences
+    """
+    assert dt >= tau, "time slide window must be larger than coinc window"
+    assert dt < dur,  "slide must be smaller than experiment duration"
+
+    raise NotImplementedError
+
+#-------------------------------------------------
 ### classes to wrap around data
+#-------------------------------------------------
 
 class Trigger(object):
     """
     wrapper around data for a single trigger
     """
-
     def __init__(self, time, amp=None):
         self.time = time
         self.amp = amp
+
+#------------------------
+
+class TrigList(list):
+    """
+    represents a time-ordered series of triggers
+    """
+    def slide(self, dt, dur):
+        """
+        perform a cyclic time shift
+        """
+        N = len(self)
+        i = 0
+        while (i<N) and (trg[i]+dt < dur): ### don't wrap around yet so just modify in place
+            self.trgs[i].time += dt
+            i += 1
+        j = i
+        while (i<N): ### does wrap around so we manipulate the placement in the list
+            trigger = self.trgs.pop(i)
+            trigger.time += dt - dur
+            self.trgs.insert(i-j, trigger)
+            i += 1
+
+#------------------------
 
 class Realization(object):
     """
     one realization of the observed triggers
     can either be fed lists of triggers or it will generate them itself.
     """
-
     def __init__(self, duration, Ndet=2):
         assert isinstance(Ndet, int), "Ndet must be an integer"
         assert Ndet > 1, "must have at least 2 detectors"
 
         self.Ndet = Ndet
         self.dur = duration
-        self.trgs = [[]]*Ndet ### instantiate empty lists initially
+        self.trgs = [None]*Ndet ### instantiate empty lists initially
 
     def simulate(self, rateC, rates):
         """
@@ -78,47 +209,25 @@ class Realization(object):
         """
         number of coincidences with window="tau"
         """
-        Ns = [self.get_dn(n) for n in xrange(self.Ndet)] ### number of triggers in each stream
-        inds = [[0,0] for _ in xrange(self.Ndet-1)] ### the indecies for each stream (besides stream 0, which we treat separately)
+        return coinc(self.trgs, tau)
 
-        count = 0
-        for trg in self.trgs[0]: ### iterate over triggers in "stream 0", counting the total number of coincs for each
-            t = trg.time ### time of interest
-
-            for n, (s, e) in enumerate(inds): ### iterate through the rest of the streams and find the idecies that bracket "t"
-                N = Ns[n] ### number of events in "stream n"
-
-                while (s<N) and (self.trgs[n][s].time < t-tau): ### start is too early, so we bump it
-                    s += 1
-
-                if e < s: ### end must be at least as big as the start
-                    e = s 
-                inds[n] = [s, e] ### update start and end points
-
-                ### ensure that the start is still a good coinc
-                if self.trgs[n][s].time > t+tau:
-                    break ### no good coincs here! so we stop working and move to the next "steam 0" trigger
-
-                while (e+1<N) and (self.trgs[n][e+1].time < t+tau): ### end time is too early
-                    e += 1
-                inds[n][1] = e ### update end point
-
-            else: ### there must be at least one participating trigger in all streams, otherwise we would have broken.
-                c = 1
-                for s, e in inds:
-                    c *= (e-s+1) ### total number of coincs is the product of the number participating in each stream
-                count += c
-
-        return count
-
-    def get_nP(self, tau):
+    def get_nP(self, tau, dt):
         """
         number of coincidences from all possible time slides keeping zero-lag coincidences with window="tau"
         """
-        raise NotImplementedError
-
+        return slideCoinc( copy.deepcopy(self.trgs), tau, dt, self.dur )
+        
     def get_nM(self, tau):
         """
         number of coincidences from all possible time slides removing zero-lag coincidences with window="tau"
         """
-        raise NotImplementedError
+        return slideCoinc( removeCoinc(copy.deepcopy(self.trgs), tau), tau, dt, self.dur )
+
+#------------------------
+
+class Data(object):
+    """
+    a representation of statistics calculated from a single realization
+    """
+    def __init__(self, dur):
+        self.dur = dur
